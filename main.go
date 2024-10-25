c2ce7a89e553be23826c7b3fc231f6a6efb2f8e0
9dl <80607793+9dl@users.noreply.github.com>
2024-10-26 00:28:02 +0200

    refactor(main): removed uncessary code

--- main.go
+++ main.go
@@ -2,28 +2,10 @@ package main
 
 import (
 	"fmt"
-	Anti_Debug "goware/pkg/detection/Anti_Debug"
+	"goware/pkg/detection/Anti_Debug"
 	"goware/pkg/detection/Anti_VM"
 	"goware/pkg/modules"
-	"log"
-	"os"
-	"path/filepath"
 )
-
-func CheckForKVM() (bool, error) {
-	badDriversList := []string{"balloon.sys", "netkvm.sys", "vioinput", "viofs.sys", "vioser.sys"}
-	for _, driver := range badDriversList {
-		files, err := filepath.Glob(filepath.Join(os.Getenv("SystemRoot"), "System32", driver))
-		if err != nil {
-			log.Printf("Error accessing system files for %s: %v", driver, err)
-			continue
-		}
-		if len(files) > 0 {
-			return true, nil
-		}
-	}
-	return false, nil
-}
 
 func main() {
 	fmt.Println()
@@ -39,4 +21,6 @@ func main() {
 	fmt.Println()
 	fmt.Println("=== Modules ===")
 	fmt.Println(fmt.Sprintf("IsAdmin: %v", modules.IsAdmin()))
+
+	fmt.Scanln()
 }

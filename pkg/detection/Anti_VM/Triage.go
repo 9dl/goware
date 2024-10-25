95e1a5d3a57114632baf19596ba9f2dc7a1b4ed5
9dl <80607793+9dl@users.noreply.github.com>
2024-10-26 00:32:13 +0200

    fix(triage): removed it since its not working

--- pkg/detection/Anti_VM/Triage.go
+++ pkg/detection/Anti_VM/Triage.go
@@ -1,24 +1,6 @@
 package Anti_VM
 
-import (
-	"os/exec"
-	"strings"
-	"syscall"
-)
-
+// TODO: Implement
 func Triage() bool {
-	cmd := exec.Command("wmic", "diskdrive", "get", "model")
-	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
-	cmdOutput, err := cmd.Output()
-	if err != nil {
-		return false
-	}
-
-	models := []string{"DADY HARDDISK", "QEMU HARDDISK"}
-	for _, model := range models {
-		if strings.Contains(string(cmdOutput), model) {
-			return true
-		}
-	}
 	return false
 }
